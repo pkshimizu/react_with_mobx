213b37b1e4fc770559643d6937a2f5c6a5fe35dd
shimizu.kenji <shimizu.kenji@noncore.net>
2019-01-03 20:43:29 +0900

    イントロダクションのToDoサンプルを実装

---
+++ src/stores/todo.js
@@ -0,0 +1,26 @@
+import {action, computed, observable} from 'mobx';
+
+class TodoStore {
+  @observable todos = [];
+
+  @computed get completedTodosCount() {
+    return this.todos.filter(todo => todo.completed === true).length;
+  }
+
+  @computed get report() {
+    if (this.todos.length === 0)
+      return "<none>";
+    return `Next todo: "${this.todos[0].task}". ` +
+      `Progress: ${this.completedTodosCount}/${this.todos.length}`;
+  }
+
+  @action addTodo(task) {
+    this.todos.push({
+      task: task,
+      completed: false,
+      assignee: null
+    });
+  }
+}
+
+export default TodoStore;
